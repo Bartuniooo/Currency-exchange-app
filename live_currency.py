a05c61a44fd2631ecfda66d59e31ce95762eee81
Bartuniooo <145339980+Bartuniooo@users.noreply.github.com>
2024-02-29 10:57:33 +0100

    delete useless file live_currency.py

--- live_currency.py
+++ live_currency.py
@@ -1,7 +1,1 @@
-import requests
 
-from_currency = str(
-    input("Enter in the currency you'd like to convert to: ")).upper()
-
-to_currency = str(
-    input("Enter in the currency you'd like to convert to: ")).upper()
